dfc331138d650a597ac4a8e65e1e9f1ab8cb2b3c
Giovanni Geraci <geraci.giovanni@gmail.com>
2022-04-15 16:23:16 +0200

    Add hello world

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 	<h1>PHP Test</h1>
 		<p><b>An Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
+	        echo "Hello World";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
 		<p> <?php phpinfo(); ?> </p> 
